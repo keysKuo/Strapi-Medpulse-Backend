44eb5892df4d84f1c66b0a4d7a641fc326a89f97
keysKuo <nkeyskuo124@gmail.com>
2024-03-29 00:03:39 +0700

    Add ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,6 @@ module.exports = {
       name: "strapi",
       script: "npm",
       args: "start",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
     },
   ],
 };
