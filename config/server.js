9a00074a3ee562bf3c28dad59ffd0c6a64b9d928
keysKuo <nkeyskuo124@gmail.com>
2024-03-29 00:44:37 +0700

    fix url

--- config/server.js
+++ config/server.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
+  url: env('MY_STRAPI_URL', 'http://localhost:1337'), // Thêm dòng này
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
