f5cfd86bbbffada598648e211ab47b9a004487a6
keysKuo <nkeyskuo124@gmail.com>
2024-03-29 09:59:45 +0700

    Fix Cors

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,8 @@
+module.exports = {
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ["https://medpulse.vn", "http://localhost:3000"], // Thêm domain của bạn vào đây
+    },
+  },
+};
