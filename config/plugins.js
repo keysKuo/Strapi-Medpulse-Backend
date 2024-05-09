c0c9f013981ebdce1bdc41ad2ee86874458085dd
keysKuo <nkeyskuo124@gmail.com>
2024-05-09 16:35:19 +0700

    Limit Upload file 100mb

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,5 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    sizeLimit: env.int("FILE_UPLOAD_SIZE_LIMIT", 100 * 1024 * 1024), // Giới hạn kích thước file (đơn vị byte), mặc định là 100MB
+  },
+});
