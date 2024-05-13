5bb893c208b1eb0510b544c7962c3c8d0306d001
Sheldon Kuo <95120517+keysKuo@users.noreply.github.com>
2024-05-13 12:21:16 +0700

    Update queryString homepage

--- queries/home.js
+++ queries/home.js
@@ -16,6 +16,9 @@ module.exports = {
         }
       }
     },
+    thong_ke_chat_luong: {
+      fields: ['number', 'text'],
+    },
     doi_tac: {
       fields: ['name'],
       populate: {
@@ -44,5 +47,5 @@ module.exports = {
 }
 
 /*
-  /api/trang-chus?populate[banner][fields][0]=heading&populate[banner][fields][1]=body&populate[banner][fields][2]=href&populate[banner][populate][background][fields][0]=url&populate[dich_vu_cung_cap][fields][0]=heading&populate[dich_vu_cung_cap][fields][1]=body&populate[dich_vu_cung_cap][fields][2]=href&populate[dich_vu_cung_cap][populate][img][fields][0]=url&populate[doi_tac][fields][0]=name&populate[doi_tac][populate][img][fields][0]=url&populate[feedbacks][fields][0]=customer&populate[feedbacks][fields][1]=major&populate[feedbacks][fields][2]=location&populate[feedbacks][fields][3]=body&populate[feedbacks][populate][img][fields][0]=url&populate[reviews][fields][0]=title&populate[reviews][fields][1]=subtitle&populate[reviews][fields][2]=href&populate[reviews][fields][3]=createdAt&populate[reviews][fields][4]=tag&populate[reviews][populate][thumb][fields][0]=url
+  /api/trang-chus?populate[banner][fields][0]=heading&populate[banner][fields][1]=body&populate[banner][fields][2]=href&populate[banner][populate][background][fields][0]=url&populate[dich_vu_cung_cap][fields][0]=heading&populate[dich_vu_cung_cap][fields][1]=body&populate[dich_vu_cung_cap][fields][2]=href&populate[dich_vu_cung_cap][populate][img][fields][0]=url&populate[thong_ke_chat_luong][fields][0]=number&populate[thong_ke_chat_luong][fields][1]=text&populate[doi_tac][fields][0]=name&populate[doi_tac][populate][img][fields][0]=url&populate[feedbacks][fields][0]=customer&populate[feedbacks][fields][1]=major&populate[feedbacks][fields][2]=location&populate[feedbacks][fields][3]=body&populate[feedbacks][populate][img][fields][0]=url&populate[reviews][fields][0]=title&populate[reviews][fields][1]=subtitle&populate[reviews][fields][2]=href&populate[reviews][fields][3]=createdAt&populate[reviews][fields][4]=tag&populate[reviews][populate][thumb][fields][0]=url
 */
